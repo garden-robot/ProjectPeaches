99c50795879bc9cba679558a58c0fa7769969ba3
jude <kap9566@nyu.edu>
2020-11-12 15:12:59 -0500

    Peaches Art + some tweaking (no changes to code)
    pushed the peaches assets, tweaked some rig stuff

---
+++ Assets/Scripts/Stabilize.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stabilize : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody = null;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+       // _rigidbody.AddForce(0, 10, 0);
+    }
+}
--- Assets/TempArt/BodyMovement.cs
+++ Assets/TempArt/BodyMovement.cs
@@ -6,10 +6,12 @@ public class BodyMovement : MonoBehaviour
 {
     [SerializeField] private string _thrustButton = "t";
     [SerializeField] private string _jumpButton = "g";
+    [SerializeField] private string _directionButton = "1";
     [SerializeField] private float _speed = 1f;
     [SerializeField] private float _jumpSpeed = 1f;
     [SerializeField] private GameObject _buttFire = null;
     [SerializeField] private GameObject _jumpFire = null;
+   // [SerializeField] private bool _direction = false;
 
     private Rigidbody2D _rigidBody = null;
     private Transform _transform = null;
@@ -23,25 +25,52 @@ public class BodyMovement : MonoBehaviour
     }
 
     void Update()
-    {
-        //THRUST
-        if(Input.GetKey(_thrustButton))
-        {
-            _buttFire.SetActive(true);
-            _rigidBody.velocity += (new Vector2(-_transform.right.x, -(_transform.right.y - 1)) * _speed);
-        }else if(Input.GetKeyUp(_thrustButton)){
-            _buttFire.SetActive(false);
-            //_rigidBody.velocity = Vector2.zero;
-        }
+    { 
+     
 
-        //JUMP
-        if(Input.GetKey(_jumpButton))
+  
+
         {
-            _jumpFire.SetActive(true);
-            _rigidBody.velocity += (new Vector2(_transform.up.x, _transform.up.y) * _jumpSpeed);
-        }else if(Input.GetKeyUp(_jumpButton)){
-            _jumpFire.SetActive(false);
-            //_rigidBody.velocity = Vector2.zero;
+
+            //THRUST
+            if (Input.GetKey(_thrustButton))
+            {
+                _buttFire.SetActive(true);
+                _rigidBody.velocity += (new Vector2(-_transform.right.x, -(_transform.right.y - 1)) * _speed);
+            }
+            else if (Input.GetKeyUp(_thrustButton))
+            {
+                _buttFire.SetActive(false);
+                //_rigidBody.velocity = Vector2.zero;
+            }
+
+            //JUMP
+            if (Input.GetKey(_jumpButton))
+            {
+                _jumpFire.SetActive(true);
+                _rigidBody.velocity += (new Vector2(_transform.up.x, _transform.up.y) * _jumpSpeed);
+            }
+            else if (Input.GetKeyUp(_jumpButton))
+            {
+                _jumpFire.SetActive(false);
+                //_rigidBody.velocity = Vector2.zero;
+            }
+            //CHANGE DIRECTION
+            /*
+            if (Input.GetKeyDown(_directionButton))
+            {
+                _direction = !_direction;
+                if (_direction == true)
+                {
+                    _rigidBody.rotation.Set(_rigidBody.rotation.x, _rigidBody.rotation.y - 180, _rigidBody.rotation.z, _rigidBody.rotation.w);
+                }
+                else
+                {
+                    _rigidBody.rotation.Set(_rigidBody.rotation.x, _rigidBody.rotation.y + 180, _rigidBody.rotation.z, trans.rotation.w);
+                }
+            }
+           */
         }
+        
     }
 }
