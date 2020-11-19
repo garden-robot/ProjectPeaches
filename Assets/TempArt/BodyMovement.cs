acf737b799d0097a6e74d549443636c12c9052b4
jude <kap9566@nyu.edu>
2020-11-19 16:03:04 -0500

    Fixed up Peaches
    Messed around with some more movement

--- Assets/TempArt/BodyMovement.cs
+++ Assets/TempArt/BodyMovement.cs
@@ -6,12 +6,13 @@ public class BodyMovement : MonoBehaviour
 {
     [SerializeField] private string _thrustButton = "t";
     [SerializeField] private string _jumpButton = "g";
-    [SerializeField] private string _directionButton = "1";
+    [SerializeField] private string _thrustButton_right = "v";
     [SerializeField] private float _speed = 1f;
     [SerializeField] private float _jumpSpeed = 1f;
     [SerializeField] private GameObject _buttFire = null;
     [SerializeField] private GameObject _jumpFire = null;
-   // [SerializeField] private bool _direction = false;
+    // [SerializeField] private bool _direction = false;
+    [SerializeField] private GameObject BodyFlip;
 
     private Rigidbody2D _rigidBody = null;
     private Transform _transform = null;
@@ -33,16 +34,44 @@ public class BodyMovement : MonoBehaviour
         {
 
             //THRUST
-            if (Input.GetKey(_thrustButton))
+            if (Input.GetKey(_thrustButton)) // left
             {
                 _buttFire.SetActive(true);
-                _rigidBody.velocity += (new Vector2(-_transform.right.x, -(_transform.right.y - 1)) * _speed);
+                /*
+                if (BodyFlip.transform.rotation.y == 180)
+                {
+                    BodyFlip.transform.Rotate(0, 0,0, Space.Self);
+                    
+                }*/
+                _rigidBody.velocity += (new Vector2(-_transform.right.x, -(_transform.right.y + 1)) * _speed);
+
             }
             else if (Input.GetKeyUp(_thrustButton))
             {
                 _buttFire.SetActive(false);
                 //_rigidBody.velocity = Vector2.zero;
             }
+            if (Input.GetKey(_thrustButton_right)) // right
+            {
+                _buttFire.SetActive(true);
+
+                /*
+                if (BodyFlip.transform.rotation.y == 0)
+                {
+                    BodyFlip.transform.Rotate(0, 180,0, Space.Self);
+                    
+                }
+                */
+                _rigidBody.velocity += (new Vector2(-_transform.right.x, (_transform.right.y -1 )) * _speed);
+            }
+            else if (Input.GetKeyUp(_thrustButton_right))
+            {
+                _buttFire.SetActive(false);
+                
+                //_rigidBody.velocity = Vector2.zero;
+            }
+
+
 
             //JUMP
             if (Input.GetKey(_jumpButton))
