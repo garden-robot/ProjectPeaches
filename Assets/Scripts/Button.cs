54996e996cab5fdd1a384ea1c93f481bb5adfd2e
Jen <bourkj@rpi.edu>
2020-11-21 22:33:54 -0500

    Begin main menu setup

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BeginButton : MonoBehaviour
+{
+    [SerializeField] private Text _text = null;
+    [SerializeField] private Vector2 _unHoverSize = Vector2.one;
+    [SerializeField] private Vector2 _hoverSize = Vector2.zero;
+    [SerializeField] private Vector2 _clickSize = Vector2.zero;
+    [SerializeField] private Color _textHoverColor = Color.white;
+    [SerializeField] private Color _textUnhoverColor = Color.white;
+
+    private RectTransform _button = null;   
+    void Awake()
+    {
+        _unHoverSize = _button.localScale;
+    }
+
+    // Update is called once per frame
+    void OnPointerEnter()
+    {
+        _button.localScale = _hoverSize;
+        _text.color = _textHoverColor;
+    }
+}
