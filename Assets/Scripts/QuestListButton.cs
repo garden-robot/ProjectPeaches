8dec85679c17ee14dcbba8ffd0d443ae3a13c70c
Jen <bourkj@rpi.edu>
2020-11-29 21:02:22 -0500

    Add quest list button and animations

---
+++ Assets/Scripts/QuestListButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestListButton : MonoBehaviour
+{
+    [SerializeField] private Animator _listAnim = null;
+
+    public void OnClick()
+    {
+        if(_listAnim.GetBool("Clicked") == false)
+        {
+            _listAnim.SetBool("Clicked", true);
+        }else {
+            _listAnim.SetBool("Clicked", false);
+        }
+    }
+}
