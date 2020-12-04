57727b60a6e1ebb740e82d56155b85e445043865
Walid <ar6749@nyu.edu>
2020-12-04 11:49:38 -0500

    Scene changers/Level designs
    Created camera changes between levels+added/redesigned all levels

---
+++ Assets/Scripts/Cameramov.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameramov : MonoBehaviour
+{
+    // public Transform cameraObject;
+    public Transform background;
+    public Vector3 cameraOffset;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Camera.main.transform.position = background.position + cameraOffset;
+        }
+    }
+}
---
+++ Assets/Scripts/ScreenMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class ScreenMover : MonoBehaviour
+//{
+//    [SerializeField] private Transform _target = null;
+
+//    private Transform player;
+
+//    public Camera backyardBackground0;
+//    public Camera backyardBackground1;
+
+//    void Start()
+//    {
+       
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if(Input.GetKeyDown("J"))
+//        {
+//            mainCamera.gameObject.SetActive(false);
+//            camera.gameObject.SetActive(true);
+//        }else
+//        {
+//            mainCamera.gameObject.SetActive(true);
+//            camera.gameObject.SetActive(false);
+//        }
+//    }
+//}
