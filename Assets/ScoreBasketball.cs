75f3ebb9a5abd124087f7939952186ead03c44a8
jude <kap9566@nyu.edu>
2020-12-05 18:29:22 -0500

    Basketballhoop, butt rocket partiles, jump rocket particles

---
+++ Assets/ScoreBasketball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBasketball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Collider2D>().tag == "pickUp")
+        {
+            Debug.Log("Score!");
+        }
+    }
+}
+
+
