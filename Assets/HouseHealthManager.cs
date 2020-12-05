5e99520a9d5fe2bb6f701adcdcacdc934765940e
Jen <bourkj@rpi.edu>
2020-12-04 23:01:07 -0500

    Add house health bar functionality

--- Assets/HouseHealthManager.cs
+++ Assets/HouseHealthManager.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HouseHealthManager : MonoBehaviour
 {
     public float currentHealth; //between 0.0% - 100.0%
     public float maxHealth = 100.0f;
     public float deadHealth = 0.0f ;
+
+    private float _initialScale = 1000f;
+    private Image _healthBar = null;
     // Start is called before the first frame update
     void Start()
     {
+        _healthBar = GetComponent<Image>();
         currentHealth = maxHealth;
+        _initialScale = _healthBar.fillAmount;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(currentHealth);
+        //set bar length based on health
+        float newX = ((currentHealth* _initialScale)/maxHealth);
+        //Vector3 newScale = new Vector3(newX, _healthBar.fillAmount.y, _healthBar.fillAmount.z);
+        _healthBar.fillAmount = newX;
+
+        //death check
         if (currentHealth == deadHealth)
         {
             //restart game
