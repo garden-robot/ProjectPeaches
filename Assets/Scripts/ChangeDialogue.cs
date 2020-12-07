a00fcb742be4e0a0ac9c7ccc940a640b586b5e5b
Jason Guisao <guisao.jason@gmail.com>
2020-12-07 18:03:46 -0500

    added dialoguechange script, empty character objects/quest items

---
+++ Assets/Scripts/ChangeDialogue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDialogue : MonoBehaviour
+{
+    [SerializeField] private Dialogue _dialogue = null;
+    [SerializeField] private string _neededTag;
+    private bool _entered = false;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == _neededTag && _entered == false)
+        {
+            _dialogue.DialogueTrigger();
+            _entered = true;
+        }
+    }
+}
